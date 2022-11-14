11d87e2f09797cab2e3de3e98b3be216a64babd6
tauhid-hasan-dev <tauhidhasan11@gmail.com>
2022-11-15 01:03:35 +0300

    login form completed

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,9 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
+    const { register, handleSubmit } = useForm();
+    const handleLogin = data => {
+        console.log(data)
+    }
+
     return (
-        <div>
-            <p>this is Login</p>
+        <div className='flex flex-col justify-center items-center py-20'>
+            <div className='w-96 border border-black p-7'>
+                <div className='flex justify-center '>
+                    <p className='text-2xl font-semibold'>Login</p>
+                </div>
+                <form onSubmit={handleSubmit(handleLogin)}>
+                    <div className="form-control w-full ">
+                        <label className="label">
+                            <span className="label-text">Email</span>
+                        </label>
+                        <input className="input input-bordered w-full " {...register("email")} placeholder="Email" type="text" />
+                    </div>
+                    <div className="form-control w-full ">
+                        <label className="label">
+                            <span className="label-text">Passowrd</span>
+                        </label>
+                        <input className="input input-bordered w-full" {...register("password")} placeholder="Password" type="password" />
+                        <label className="label">
+                            <small className="label-text">Forget Password?</small>
+                        </label>
+                    </div>
+                    <input className='btn  w-full btn-accent' type="submit" />
+                </form>
+                <p className='text-sm mt-3'>New to doctors portal? <span className='text-secondary text-sm underline'><Link to='/signup' >Create new account</Link></span> </p>
+                <div className="divider">OR</div>
+                <button className='btn btn-outline w-full'>CONTINUE WITH GOOGLE</button>
+            </div>
         </div>
     );
 };
