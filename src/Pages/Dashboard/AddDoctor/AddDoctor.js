08013400584612f9f2883758c82756999e41d915
tauhid-hasan-dev <tauhidhasan11@gmail.com>
2022-11-19 22:01:42 +0300

    all doctors data loaded

--- src/Pages/Dashboard/AddDoctor/AddDoctor.js
+++ src/Pages/Dashboard/AddDoctor/AddDoctor.js
@@ -26,7 +26,7 @@ const AddDoctor = () => {
         const formData = new FormData();
         console.log(image);
         formData.append('image', image);
-        const url = `https://api.imgbb.com/1/upload?expiration=600&key=${imageHostKey}`;
+        const url = `https://api.imgbb.com/1/upload?&key=${imageHostKey}`;
 
         fetch(url, {
             method: 'POST',
--- src/Pages/Dashboard/ManageDoctors/ManageDoctors.js
+++ src/Pages/Dashboard/ManageDoctors/ManageDoctors.js
@@ -1,9 +1,78 @@
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
+import Loading from '../../Shared/Loading/Loading';
 
 const ManageDoctors = () => {
+
+    const { data: doctors = [], isLoading } = useQuery({
+        queryKey: ['doctors'],
+        queryFn: async () => {
+            try {
+                const res = await fetch('http://localhost:5000/doctors', {
+                    headers: {
+                        authorization: `bearer ${localStorage.getItem('accessToken')}`
+                    }
+                });
+                const data = await res.json();
+                return data;
+            }
+            catch (error) {
+
+            }
+        }
+    });
+
+    if (isLoading) {
+        return <Loading></Loading>
+    }
+
+
     return (
         <div>
-            <p>this is the manage doctors</p>
+            <h2 className="text-3xl">Manage Doctors: {doctors?.length}</h2>
+            <div className="overflow-x-auto">
+                <table className="table w-full">
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Avatar</th>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Specialty</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            doctors.map((doctor, i) => <tr key={doctor._id}>
+                                <th>{i + 1}</th>
+                                <td><div className="avatar">
+                                    <div className="w-24 rounded-full">
+                                        <img src={doctor.image} alt="" />
+                                    </div>
+                                </div></td>
+                                <td>{doctor.name}</td>
+                                <td>{doctor.email}</td>
+                                <td>{doctor.specialty}</td>
+                                <td>
+                                    <label /* onClick={() => setDeletingDoctor(doctor)}  */ htmlFor="confirmation-modal" className="btn btn-sm btn-error">Delete</label>
+                                </td>
+                            </tr>)
+                        }
+                    </tbody>
+                </table>
+            </div>
+            {/* {
+                deletingDoctor && <ConfirmationModal
+                    title={`Are you sure you want to delete?`}
+                    message={`If you delete ${deletingDoctor.name}. It cannot be undone.`}
+                    successAction={handleDeleteDoctor}
+                    successButtonName="Delete"
+                    modalData={deletingDoctor}
+                    closeModal={closeModal}
+                >
+                </ConfirmationModal>
+            } */}
         </div>
     );
 };
